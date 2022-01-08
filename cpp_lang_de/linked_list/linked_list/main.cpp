f4e5f88f594c5c2ac81149c2f0fa1b76e7104e16
iago <xxiagointer@gmail.com>
2022-01-07 23:43:19 -0300

    add linked list class implementation and methods print_list add_node

--- cpp_lang_de/linked_list/linked_list/main.cpp
+++ cpp_lang_de/linked_list/linked_list/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 class Node{
     private: 
@@ -6,19 +8,66 @@ class Node{
         Node* next;
     
     public:
-        Node (int value){
+        Node (int value)
+        {
             this->value = value; 
             this->next = nullptr;
         }
         int get_value(){return this->value;}
-        void set_next(Node n){this->next = n;}
+        void set_next(Node* n){this->next = n;}
+        Node* get_next(){return this->next;}
 };
 
+class LinkedList{
+    private:
+        int size;
+        Node * root;
+
+    public:
+        LinkedList()
+        {
+            this->size = 0;
+            this->root = nullptr;
+        }
+        void add_node(Node* n)
+        {
+            Node * tmp = this->root;
+
+            if(this->root == nullptr)
+                this->root = n;
+            else
+            {
+                while(tmp->get_next() != nullptr);
+                tmp->set_next(n);
+            }                                
+            this->size++;
+        }
+        void print_list()
+        {
+            Node* tmp = this->root;
+            string a = "[";            
+            while(tmp != nullptr)
+            {
+                a += to_string(tmp->get_value());
+                tmp = tmp->get_next();
+                if(tmp)
+                    a += ", ";
+            }
+            a += "]";
+            std::cout << a << std::endl;
+        }
+};
 
 int main(){
-    Node a = Node(3);
+    Node* a = new Node(3);
+    Node* b = new Node(42);
+    LinkedList* ll = new LinkedList();
+    ll->add_node(a);
+    ll->add_node(b);
 
-    std::cout << a.get_value() << std::endl;
+    ll->print_list();
+
+    //std::cout << a.get_value() << std::endl;
     return 0;
     
 }
